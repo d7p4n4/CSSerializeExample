6d4182729f8302c24d65cebd3f4cb0cfb8f46677
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-17 07:39:44 +0100

    Add project files.

---
+++ CSSerializeExample/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSerializeExample
+{
+    public class Employee
+    {
+        public int Id = 1;
+        public String name = "John Smith";
+        public string subject = "Physics";
+        public List<int> list = new List<int>() { 10, 20, 30 };
+    }
+}
---
+++ CSSerializeExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace CSSerializeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bs = new Employee();
+
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+
+            TextWriter txtWriter = new StreamWriter("d:\\Server\\Visual_studio\\Serialization.xml");
+
+            xs.Serialize(txtWriter, bs);
+
+            txtWriter.Close();
+
+            using (FileStream fileStream = new FileStream("d:\\Server\\Visual_studio\\Serialization.xml", FileMode.Open))
+            {
+                Employee result = (Employee)xs.Deserialize(fileStream);
+                Console.WriteLine(result.name);
+            }
+
+        }
+    }
+}
